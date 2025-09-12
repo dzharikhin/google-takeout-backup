3686bd6d381933b294f9271ec37d169c6ef8b87b
d.zharikhin <d.zharikhin@hh.ru>
2025-09-12 13:00:05 +0300

    allow to run auth on same node as backup

--- README.md
+++ README.md
@@ -23,10 +23,11 @@ Browser server services should interact via secure network exposing only one pub
 You can run browser server on a dedicated node if it is connected to backup server
 1. go to [browser-server](./browser-server)
 2. `docker network create --opt encrypted --attachable secure_net`
-3. `docker-compose up browser-manual`
-   > if GUI display is not available on `browser-server` node - you can launch the script on any other device, auth data is compatible between different browser instances
+3. `USER_E="{your account email}" USER_P=$(read -rsp "Pswd: " p && echo $p) && docker-compose up browser-manual`
+   > if GUI display is available on `browser-server` node - it may be more reasonable to call [script](browser-server/manual_auth.py) directly, without docker with param `--skip_automation` to authorize manually
    > 
-   > it may be more reasonable to call [script](browser-server/manual_auth.py) directly, without docker
+   > otherwise if something goes wrong with auth automation on `browser-server` node - you can launch the script on any other device, auth data is compatible between different browser instances
+
    > the script sets up browser to use `wayland` - if you need `X11` - tweak the script(browser launch args)
 4. authorize, then just close browser, copy auth data(from console output)
 5. `docker-compose down browser-manual`
--- browser-server/manual_auth.py
+++ browser-server/manual_auth.py
@@ -1,12 +1,14 @@
 import asyncio
 import os
 import pathlib
+import random
+import sys
 import tempfile
 
 from playwright.async_api import async_playwright
 
 
-async def main():
+async def main(skip_automation: bool = False):
     manual_auth_wait = [1]
 
     async def handle_manual_auth_close(page):
@@ -21,18 +23,71 @@ async def main():
 
     async with async_playwright() as playwright:
         browser = await playwright.chromium.launch(
-            args=[
-                "--disable-blink-features=AutomationControlled",
-                "--ozone-platform=wayland",
+            args=sum(
+                [
+                    [
+                        "--disable-blink-features=AutomationControlled",
+                        "--no-sandbox",
+                        "--disable-web-security",
+                        "--disable-infobars",
+                        "--disable-extensions",
+                        "--start-maximized",
+                    ]
+                ],
+                ["--ozone-platform=wayland"] if skip_automation else [],
+            ),
+            ignore_default_args=[
+                "--disable-component-extensions-with-background-pages"
             ],
             headless=bool(os.getenv("RUN_HEADLESS", "False").lower() == "True".lower()),
         )
-        page = await browser.new_page()
+        page = await browser.new_page(
+            viewport={"width": 1280, "height": 1024},
+            user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+        )
         page.on("close", handle_manual_auth_close)
         await page.goto("https://takeout.google.com/settings/takeout/custom/photos")
+        if not skip_automation:
+            if page.url.startswith("https://accounts.google.com/v3/signin"):
+                email = os.getenv("USER_E")
+                await page.focus(selector="input[type=email]")
+                await page.type(
+                    selector="input[type=email]",
+                    text=email,
+                    delay=random.randint(11, 49),
+                )
+                await page.wait_for_timeout(1666)
+                await page.locator(f"button#identifierNext").or_(
+                    page.locator(f"div#identifierNext")
+                ).click()
+                await page.wait_for_timeout(5000)
+                if not await page.get_by_text("Try again", exact=True).is_hidden():
+                    pathlib.Path("/app/browser-downloads/html").write_text(
+                        await page.content()
+                    )
+                    await page.screenshot(path="/app/browser-downloads/error_page.jpg")
+                    return
+                password = os.getenv("USER_P")
+                await page.screenshot(path="/app/browser-downloads/password_page.jpg")
+                await page.type(
+                    selector="input[type=password]",
+                    text=password,
+                    delay=random.randint(11, 49),
+                )
+                await page.wait_for_timeout(1537)
+                await page.locator(f"button#passwordNext").or_(
+                    page.locator(f"div#passwordNext")
+                ).click()
+            if page.url.startswith("https://accounts.google.com/v3/signin/challenge"):
+                # await page.screenshot(path="/app/browser-downloads/2fa_page.jpg")
+                await page.locator(f'div[data-challengetype="39"]').click()
+                await page.wait_for_url(
+                    "https://takeout.google.com/settings/takeout/custom/photos"
+                )
+                await handle_manual_auth_close(page)
         while manual_auth_wait:
             await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main("--skip_automation" in sys.argv))
