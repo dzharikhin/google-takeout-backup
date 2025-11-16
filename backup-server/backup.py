5c33f52db594ee07b35f98ccacde8d35dc8abace
d.zharikhin <d.zharikhin@hh.ru>
2025-11-16 20:30:58 +0300

    tweakable global timeout

--- backup-server/backup.py
+++ backup-server/backup.py
@@ -107,6 +107,7 @@ async def main():
                 viewport={"width": 1280, "height": 1024},
                 user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
             )
+            page.set_default_timeout(TIMEOUT_MILLIS)
             async with page:
                 console = []
 
