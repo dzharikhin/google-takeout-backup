3f7657367bcd915af2fd518c6e1a7e2ca66f8c80
d.zharikhin <d.zharikhin@hh.ru>
2026-04-11 14:14:27 +0300

    persist helper tool

--- backup-server/backup-entry.sh
+++ backup-server/backup-entry.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 pip install --root-user-action=ignore --disable-pip-version-check -q playwright==${PLAYWRIGHT_VERSION}
-curl -s -L --output /app/gpth https://github.com/TheLastGimbus/GooglePhotosTakeoutHelper/releases/download/v3.4.3/gpth-linux
+if [ ! -f "/app/utils/gpth" ]; then
+    curl -L --output /app/utils/gpth https://github.com/TheLastGimbus/GooglePhotosTakeoutHelper/releases/download/v3.4.3/gpth-linux
+fi
 # ls /app
 chmod +x /app/gpth
 export PATH=$PATH:/app
